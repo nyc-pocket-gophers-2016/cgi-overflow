88b5a9652f81f74d27c8ad37833eaa5042b1da68
Gregor Destrée <email": null>
2016-06-01 11:17:07 -0400

    Create question models and migration

---
+++ app/models/question.rb
@@ -0,0 +1,7 @@
+class Question < ActiveRecord::Base
+  # Remember to create a migration!
+  has_many :answers
+  has_many :votes, :as => :votable
+  has_many :comments, :as=> :commentable
+  belongs_to :user
+end

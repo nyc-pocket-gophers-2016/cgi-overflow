bc4013bdd3a99532116ea243c6a7943db928de1b
Gregor Destrée <email": null>
2016-06-01 12:40:59 -0400

    update seed data

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+users = [
+  {first_name: 'Gregor',
+   username: 'gregor',
+   email: 'gregor@gregor.com'
+   password: 'gregor'},
+
+  {first_name: 'Calvin',
+   username: 'calvin',
+   email: 'calvin@calvin.com'
+   password: 'calvin'},
+
+  {first_name: 'Ilya',
+   username: 'ilya',
+   email: 'ilya@ilya.com'
+   password: 'ilya'}
+]
+
+User.create(users)
+
+

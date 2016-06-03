99e39e9dac265a148eb3c52dc203e5d3ac3f0c20
Ilya Yampolsky <cfnils@gmail.com>
2016-06-03 16:38:05 -0400

    first rebase point

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,14 +1,26 @@
 get '/questions/:id/comments/new' do
   @question = Question.find_by(id: params[:id])
-  erb :'comments/question-comments'
+  if logged_in?
+    if request.xhr?
+      erb :'comments/question-comments',layout: false
+    else
+      erb :'comments/question-comments'
+    end
+  else
+    redirect '/login'
+  end
 end
 
 post '/questions/:id/comments' do
   @question = Question.find_by(id: params[:id])
   @comment = @question.comments.new(comment: params[:comment], user_id: session["user_id"])
     if @comment.save
+      if request.xhr?
+        @comment.comment
+      else
       #erb :'comment/new'
       redirect "/questions/#{@question.id}"
+      end
     else
       @error = "Your comment is invalid."
       erb :'comments/question-comments'
@@ -17,15 +29,23 @@ end
 
 get '/answers/:id/comments/new' do
   @answer = Answer.find_by(id: params[:id])
-  erb :'comments/answer-comments'
+   if request.xhr?
+      erb :'comments/answer-comments',layout: false
+    else
+      erb :'comments/answer-comments'
+    end
 end
 
 post '/answers/:id/comments' do
   @answer = Answer.find_by(id: params[:id])
   @comment = @answer.comments.new(comment: params[:comment], user_id: session["user_id"])
     if @comment.save
+      if request.xhr?
+        @comment.comment
+      else
       #erb :'comment/new'
-      redirect "/questions/#{@answer.question_id}"
+        redirect "/questions/#{@answer.question_id}"
+      end
     else
       @error = "Your comment is invalid."
       erb :'comments/answer-comments'
